70b9fd586bfa22cd52e9444be96fd4f1eba35d3a
AleksMishin <shmurge3@gmail.com>
2025-07-10 22:52:23 +0300

    Mishin Aleksei: Lesson 19 homework update 4

--- homework/aleksei_mishin/lesson_19/tests.py
+++ homework/aleksei_mishin/lesson_19/tests.py
@@ -2,24 +2,45 @@ import pytest
 from payloads import OBJ_1, OBJ_2, OBJ_3, OBJ_3_1, OBJ_3_2, OBJ_4, RANDOM_OBJ
 from api_request import post_object, put_object, get_all_objects_list, patch_object, delete_object
 from assertions import object_should_be_in_list, object_should_not_be_in_list
-from fixtures import (
-    print_before_after,
-    get_start_and_stop_testing,
-    create_object,
-    create_and_delete_object
-)
+
+
+@pytest.fixture(scope='session')
+def get_start_and_stop_testing():
+    print('\nStart testing!')
+    yield
+    print('\nTesting completed!')
+
+
+@pytest.fixture(autouse=True)
+def print_before_after():
+    print('\nbefore test')
+    yield
+    print('\nafter test')
+
+
+@pytest.fixture()
+def create_and_delete_object():
+    req = post_object(RANDOM_OBJ)
+    yield req
+    delete_object(req.id)
+
+
+@pytest.fixture()
+def create_object():
+    req = post_object(RANDOM_OBJ)
+    return req
 
 
 @pytest.mark.critical
 @pytest.mark.parametrize('obj', [OBJ_1, OBJ_2, OBJ_3])
-def test_create_object(get_start_and_stop_testing, print_before_after, obj):
+def test_create_object(get_start_and_stop_testing, obj):
     response = post_object(obj)
     obj_list = get_all_objects_list()
 
     object_should_be_in_list(obj_list.data, response)
 
 
-def test_update_object(create_and_delete_object, print_before_after):
+def test_update_object(create_and_delete_object):
     update_resp = put_object(create_and_delete_object.id, RANDOM_OBJ)
     obj_list = get_all_objects_list()
 
@@ -27,7 +48,7 @@ def test_update_object(create_and_delete_object, print_before_after):
 
 
 @pytest.mark.medium
-def test_update_part_of_object(create_and_delete_object, print_before_after):
+def test_update_part_of_object(create_and_delete_object):
     update_resp_1 = patch_object(create_and_delete_object.id, OBJ_3_1)
     obj_list_1 = get_all_objects_list()
 
@@ -44,7 +65,7 @@ def test_update_part_of_object(create_and_delete_object, print_before_after):
     object_should_be_in_list(obj_list_3.data, update_resp_3)
 
 
-def test_remove_object(create_object, print_before_after):
+def test_remove_object(create_object):
     delete_object(create_object.id)
     obj_list = get_all_objects_list()
 
